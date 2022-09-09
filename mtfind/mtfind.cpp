0f6db0a6890bca9e735ed52e492091bb8d9c636c
4azoff <m.v.chazov@gmail.com>
2022-09-09 19:17:18 +0300

    Загрузка файлов проекта для дальнейшей доработки

---
+++ mtfind/mtfind.cpp
@@ -0,0 +1,110 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <thread>
+#include <map>
+#include <boost/asio/thread_pool.hpp>
+#include <boost/asio.hpp>
+#include <atomic>
+#include <mutex>
+
+std::mutex answersMutex;
+
+void FindMask(const std::string line, const std::string& mask, const int lineNumber,
+    std::atomic<int>& countAns, std::vector<std::pair<int, std::string>>& answers){
+    std::string ans;
+    for (size_t i = 0; i < line.size(); i++) {
+        auto j = i;
+        size_t h = 0;
+        while (h < mask.size() && j < line.size() && 
+            (line[j] == mask[h] || mask[h] == '?')) {
+            ans += line[j];
+            j++, h++;
+        }
+        if (ans.size() == mask.size()) {
+            countAns.fetch_add(1);
+            std::lock_guard<std::mutex> lockit(answersMutex);
+            answers.push_back(std::make_pair(lineNumber, std::to_string(lineNumber) + " " + std::to_string(i + 1) +
+                " " + ans + "\n"));
+            ans.clear();
+            i = j - 1; 
+        }
+        else {
+            ans.clear();
+        }
+    }
+}
+
+bool checkFlags(char* argv[]) {
+    
+    auto result = true;
+    
+    if (!argv[1]) {
+        std::cout << "Specifay a file \n";
+        result = false;
+    }
+    else
+    {
+        std::ifstream in(argv[1]);
+        if (!in)
+        {
+            std::cout << "Cannot open file '" << argv[1] <<
+                "' invalid path or filename \n";
+            result = false;
+        }
+        in.close();
+    }    
+
+    if (!argv[2])
+    {
+        std::cout << "Invalid mask \n";
+        result = false;
+    }
+    else {
+        std::string mask(argv[2]);
+
+        if (mask.empty())
+        {
+            std::cout << "Invalid mask \n";
+            result = false;
+        }
+    }
+    
+    return result;
+}
+
+int main(int argc, char* argv[])
+{
+    if (!checkFlags(argv)) {
+        return 0;
+    }
+    std::ifstream in(argv[1]);
+    std::string mask(argv[2]);
+
+    std::string line;
+    int lineNumber = 0;
+    std::atomic<int>countAns{ 0 };      
+    std::vector<std::pair<int, std::string>> answers;
+
+    boost::asio::thread_pool pool(std::thread::hardware_concurrency());   
+
+    if (in.is_open())
+    {
+        while (getline(in, line))
+        {
+            lineNumber++;
+            boost::asio::post(pool, std::bind(FindMask, line, 
+                std::ref(mask), lineNumber, std::ref(countAns), std::ref(answers)));           
+        }
+    }
+    in.close();     
+    pool.join();
+
+    std::cout << countAns << "\n";
+    std::sort(answers.begin(), answers.end());
+    for (auto el : answers) {
+        std::cout << el.second;
+    }
+
+    return 0;
+}
